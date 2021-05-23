347b9331bffb93e08b612b8005b754052161d977
Yarramsetty siva satyanarayana <64882708+siva6160@users.noreply.github.com>
2021-05-23 19:43:29 +0530

    Add files via upload

---
+++ vari.java
@@ -0,0 +1,17 @@
+class vari {
+
+	public static void main(String[] args) {
+		int a=15;
+		int b=15;
+		int c=a+b;
+		int d=a-b;
+		int e=a/b;
+		int f=a%b;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+
+	}
+
+}
